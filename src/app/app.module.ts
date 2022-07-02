fc06f4c64039b5d39264f251efc21906a071d04f
Antônio Maurício <antoniomauricio3469@gmail.com>
2022-07-02 20:05:24 -0300

    Criação de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { TalksComponent } from './talks/talks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    HomeComponent,
+    RegisterComponent,
+    TalksComponent
   ],
   imports: [
     BrowserModule,
