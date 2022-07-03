08d64299a5f12ecede130b6f22a41f2da0956e7e
Antônio Maurício <antoniomauricio3469@gmail.com>
2022-07-02 21:34:48 -0300

    Alterações na classe app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { TalksComponent } from './talks/talks.component';
 
-const routes: Routes = [];
+const routes: Routes =
+[
+  {path: 'home', component: HomeComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'sign-in', component: SignInComponent},
+  {path: 'talks', component: TalksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
